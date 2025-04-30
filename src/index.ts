67f4f1a741a9b5e2f7d078afdc57062ac4b123c0
Aaron Strick <aaronstrick@gmail.com>
2025-04-29 22:50:00 -0700

    (no commit message provided)

--- src/index.ts
+++ src/index.ts
@@ -144,8 +144,6 @@ function updateSynthParameter(param: string, value: number) {
             synth.volume.value = minVolume + (maxVolume - minVolume) * logValue;
         case 'detune':
             tilt.setGain(mapDetuneValue(value))
-            // Map 0-100 to 0-10 for the Q value of the bandpass filter
-            bandpassFilter.Q.value = (value / 100) * 10;
             break;
         case 'attack':
             // Map 0-100 to a frequency range for the bandpass filter
@@ -156,6 +154,8 @@ function updateSynthParameter(param: string, value: number) {
             bandpassFilter.frequency.value = logFreq;
             break;
         case 'release':
+            // Map 0-100 to 0-10 for the Q value of the bandpass filter
+            bandpassFilter.Q.value = (value / 100) * 10;
             break;
     }
 }
