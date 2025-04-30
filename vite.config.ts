b6d109297537cf470807e47cbb117ba51f42e2a2
Aaron Strick <aaronstrick@gmail.com>
2025-04-30 00:07:45 -0700

    fix viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: 'src',
-    server: {
-        open: true,
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: 'src/index.html',
     },
 });
